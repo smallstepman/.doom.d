8f4c103937716da23708602c3285f3117369bd7e
Marcin Nowak-Liebiediew <m.liebiediew@gmail.com>
2022-03-03 16:44:30 +0100

    org: reorganize (part 1)

--- custom.el
+++ custom.el
@@ -3,6 +3,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-structure-template-alist
+   '(("n" . "notes")
+     ("a" . "export ascii")
+     ("c" . "center")
+     ("C" . "comment")
+     ("e" . "example")
+     ("E" . "export")
+     ("h" . "export html")
+     ("l" . "export latex")
+     ("q" . "quote")
+     ("s" . "src")
+     ("v" . "verse")
+     ("r" . "src rust :tangle \"/tmp/org-babel-rust/src/main.rs\" :exports both :toolchain 'nightly :main")))
  '(package-selected-packages '(org-anki))
  '(safe-local-variable-values
    '((eval add-hook 'after-save-hook
